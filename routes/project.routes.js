dbf804cacc644e47383d4d3eae99e2a9364607ec
Ap00rvx <apurvabraj419@gmail.com>
2024-12-28 16:35:34 +0530

    delete project routes

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {  createProject,getAllProjects,getProject, updateProject} = require('../controller/projects.controller')
+const {  createProject,getAllProjects,getProject, updateProject,deleteProject} = require('../controller/projects.controller')
 const adminMiddleware = require('../middleware/admin.middleware')
 const router = express.Router()
 
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post('/create', adminMiddleware, createProject);
 router.get('/', getAllProjects);
 router.get('/:id', getProject);
+router.delete("/:id", deleteProject);
 router.post('/:id',adminMiddleware, updateProject);
 
 module.exports = router
